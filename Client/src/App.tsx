ae4371569f89e918a4b457cdd1227d78b42d1ae0
edgarlevyings44 <edgarlevy81@gmail.com>
2024-04-02 00:39:25 +0300

    achieve routing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './pages/homepage'
 
 function App() {
   
-  return (
-    <> 
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+  return ( 
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+    
   )
 }
 
---
+++ Client/src/pages/homepage.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  
+    return (
+      <> 
+      <h1 className="text-3xl font-bold underline">
+        Homepage
+      </h1>
+      </>
+    )
+  }
+  
+  export default Navbar
+  
