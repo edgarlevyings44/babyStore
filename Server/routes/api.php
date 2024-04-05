e9fb7e6892dcd006d15f7a96f5f3016786817c7e
kibigo <jacksonkibigo99@gmail.com>
2024-04-05 22:48:09 +0300

    added more api on laravel

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+    Route::get('/profile', [UserController::class, 'profile']);
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::put('/forgot', [UserController::class, 'forgotPassword']);
 });
 
 
 Route::get('/products', [ProductsController::class, 'getProduct']);
+Route::get('/products/{id}', [ProductsController::class, 'singleProduct']);
+
+Route::post('/createuser', [UserController::class, 'createUser']);
+Route::post('/login', [UserController::class, 'login']);
+
