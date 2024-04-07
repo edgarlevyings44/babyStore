b90b9380094015822fca1b2c83802fff6c9cb072
kibigo <jacksonkibigo99@gmail.com>
2024-04-07 17:05:21 +0300

    added post product api

--- Server/app/Http/Controllers/ProductsController.php
+++ Server/app/Http/Controllers/ProductsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Products;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductsController extends Controller
 {
@@ -28,6 +29,41 @@ class ProductsController extends Controller
 
         return response()->json([
             $singleProduct
+        ]);
+    }
+
+
+    //Admin add products
+
+    public function addProduct(Request $request)
+    {
+        $validator = Validator::make($request->only('name', 'description', 'image_url', 'price','quantity', 'category'),[
+            'name' => ['required', 'max:50', 'string'],
+            'description' => ['required', 'string'],
+            'image_url' => ['required', 'string'],
+            'price' => ['required', 'integer'],
+            'quantity' => ['required', 'integer'],
+            'category' => ['required', 'string']
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json([
+                $validator->errors()
+            ]);
+        }
+
+        $product = Products::create([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'image_url' => $request->input('image_url'),
+            'price' => $request->input('price'),
+            'quantity' => $request->input('quantity'),
+            'category' => $request->input('category')
+        ]);
+
+        return response()->json([
+            "Message" => "Product Added"
         ]);
     }
 }
