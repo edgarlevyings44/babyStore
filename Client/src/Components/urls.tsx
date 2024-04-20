63fb29e65c2b49648290ad06b010672744e3a24f
edgarlevyings44 <edgarlevy81@gmail.com>
2024-04-20 16:06:01 +0300

    productUrl

--- Client/src/Components/urls.tsx
+++ Client/src/Components/urls.tsx
@@ -2,3 +2,5 @@ const BASE_URL = "http://127.0.0.1:8000/api";
 
 export const loginUser = `${BASE_URL}/login`;
 export const registerUser = `${BASE_URL}/register`;
+
+export const productsUrl = `${BASE_URL}/products`
