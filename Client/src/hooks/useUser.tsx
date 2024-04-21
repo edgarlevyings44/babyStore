a68f63ce810efacc6ef9ed94cb73acf1cf662cfa
edgarlevyings44 <edgarlevy81@gmail.com>
2024-04-21 21:36:58 +0300

    parsist data on login

--- Client/src/hooks/useUser.tsx
+++ Client/src/hooks/useUser.tsx
@@ -4,7 +4,7 @@ export const useUser = (): UserContextProps => {
     const context = useContext(UserContext);
 
     if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider')
+        throw new Error('useUser must be used within an UserProvider')
     }
 
     return context
