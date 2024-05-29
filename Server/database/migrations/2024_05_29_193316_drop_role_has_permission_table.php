6649df8c0225092cfaaa12cc91648fe06cc121e4
kibigo <jacksonkibigo99@gmail.com>
2024-05-30 01:11:59 +0300

    modifying users table

--- Server/database/migrations/2024_05_29_193316_drop_role_has_permission_table.php
+++ Server/database/migrations/2024_05_29_193316_drop_role_has_permission_table.php
@@ -11,9 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->integer('user_type')->default(0);
-        });
+        Schema::dropIfExists('roles_has_permission');
     }
 
     /**
@@ -21,8 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        //
     }
 };
--- Server/database/migrations/2024_05_29_194231_add_user_type_column_into_users_table.php
+++ Server/database/migrations/2024_05_29_194231_add_user_type_column_into_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('user_type')->default(0);
+            $table->integer('user_type')->nullable();
         });
     }
 
@@ -21,8 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        //
     }
 };
